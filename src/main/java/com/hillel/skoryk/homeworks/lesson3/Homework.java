537f0481917f867c9d513721f8293d4c199d7102
SkorykAnna <116644325+SkorykAnna@users.noreply.github.com>
2022-10-25 07:10:02 -0500

    Update Homework.java
    commit from git

--- src/main/java/com/hillel/skoryk/homeworks/lesson3/Homework.java
+++ src/main/java/com/hillel/skoryk/homeworks/lesson3/Homework.java
@@ -3,5 +3,7 @@ package com.hillel.skoryk.homeworks.lesson3;
 public class Homework {
     public static void main(String[] args) {
         System.out.println("Hi, Git! (not from Germany one)");
+        
+        //another way to check on
     }
 }
