c17e90f5f14b6704b04ef10d3ad054f9a381981e
Skoryk Anna <boccamanga@gmail.com>
2022-10-25 07:09:08 -0500

    Commit sout

--- src/main/java/com/hillel/skoryk/homeworks/lesson3/Homework.java
+++ src/main/java/com/hillel/skoryk/homeworks/lesson3/Homework.java
@@ -2,6 +2,6 @@ package com.hillel.skoryk.homeworks.lesson3;
 
 public class Homework {
     public static void main(String[] args) {
-        System.out.println("Hi, Git! / not from Germany one");
+        System.out.println("Hi, Git! (not from Germany one)");
     }
 }
