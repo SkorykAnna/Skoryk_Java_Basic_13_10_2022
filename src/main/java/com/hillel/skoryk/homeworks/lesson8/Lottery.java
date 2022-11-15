9b6d7112daf73389c9bcaffc094a8cba7f7fdc97
Skoryk Anna <boccamanga@gmail.com>
2022-11-15 22:25:05 +0100

    homework

---
+++ src/main/java/com/hillel/skoryk/homeworks/lesson8/Lottery.java
@@ -0,0 +1,40 @@
+package com.hillel.skoryk.homeworks.lesson8;
+
+import java.util.Arrays;
+
+public class Lottery {
+
+    public static void main(String[] args) {
+
+        int n = 7;
+        int count = 0;
+
+        int [] arrCompanyNumbers = array(n);
+        int [] arrUserNumbers = array(n);
+
+        Arrays.sort(arrCompanyNumbers);
+        Arrays.sort(arrUserNumbers);
+
+        for (int i = 0; i < n; i++) {
+            if (arrCompanyNumbers[i] == arrUserNumbers[i]) {
+                count++;
+            }
+        }
+
+        System.out.println(Arrays.toString(arrCompanyNumbers) + " числа, які загадані фірмою-організатором лотереї.");
+        System.out.println(Arrays.toString(arrUserNumbers) + " числа, які вказав гравець.");
+        System.out.println();
+        System.out.println("Кількість збігів: " + count);
+    }
+
+    public static int[] array(int n) {
+
+        int [] array = new int [n];
+
+        for (int i = 0; i < n; i++) {
+            array[i] = (int) (Math.random() * 10);
+        }
+        return array;
+    }
+
+}
